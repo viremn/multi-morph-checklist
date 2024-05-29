c517bb118ee3adf6dd3930fec1448a6b7a44ed9c
viremn <viremannen@gmail.com>
2024-05-29 20:35:22 +0200

    eval continues

--- eval.py
+++ eval.py
@@ -21,25 +21,25 @@ def get_answer_variations(data, lang=None, task=None):
         elif lang == 'fi':
             answer_variations = fi_negation(data)
         elif lang == 'ja':
-            pass
+            answer_variations = ja_negation2(data)
     elif task == 'numerals1':
         if lang == 'en':
-            pass
+            answer_variations = en_numerals1(data)
         elif lang == 'de':
-            pass
+            answer_variations = de_numerals1(data)
         elif lang == 'fi':
-            pass
+            answer_variations = fi_numerals1(data)
         elif lang == 'ja':
-            pass
+            answer_variations = ja_numerals1(data)
     elif task == 'numerals2':
         if lang == 'en':
-            pass
+            answer_variations = en_numerals2(data)
         elif lang == 'de':
-            pass
+            answer_variations = de_numerals2(data)
         elif lang == 'fi':
-            pass
+            answer_variations = fi_numerals2(data)
         elif lang == 'ja':
-            pass
+            answer_variations = ja_numerals2(data)
     elif task == 'numerals2':
         if lang == 'en':
             pass
@@ -234,3 +234,203 @@ def ja_negation1(data):
     return {'perfect': perfect_matches, 
             'positive': positive_matches,
             'negative': negative_matches}
+
+def ja_negation2(data):
+    conj = 'と'
+    cop = 'でございます' if data['answer'].endswith('でございます。') else 'です' if data['answer'].endswith('です。') else ''
+
+    subjects = data['answer'].strip('。')[:-len(cop)] if cop != '' else data['answer'].strip('。 ')
+    subjects = [subjects, conj.join(subjects.split(conj)[::-1])] if conj in subjects else [subjects]
+
+    predicate = data['question'].rstrip('か。？?').split('のは')[0].replace('じゃない', '').replace('でない', '')
+
+    non_subjects = data['context'].split('は')[0]
+    non_subjects = [non_subjects, conj.join(non_subjects.split(conj)[::-1])] if conj in non_subjects else [non_subjects]
+
+    perfect_matches = []
+    positive_matches = []
+    negative_matches = []
+    for subject in subjects:
+        if cop == '':
+            perfect_matches.append(f"{subject}")
+            perfect_matches.append(f"{subject}だ")
+
+            positive_matches.append(f"{subject}が{predicate}じゃない")
+            positive_matches.append(f"{subject}が{predicate}ではない")
+
+            positive_matches.append(f"{predicate}じゃないのは{subject}だ")
+            positive_matches.append(f"{predicate}でないのは{subject}だ")
+
+        elif cop == 'です':
+            perfect_matches.append(f"{subject}です")
+
+            positive_matches.append(f"{subject}が{predicate}じゃありません")
+            positive_matches.append(f"{subject}が{predicate}ではありません")
+            positive_matches.append(f"{subject}が{predicate}じゃないです")
+            positive_matches.append(f"{subject}が{predicate}ではないです")
+
+            positive_matches.append(f"{predicate}でないのは{subject}です")
+            positive_matches.append(f"{predicate}じゃないのは{subject}です")
+
+        elif cop == 'でございます':
+            perfect_matches.append(f"{subject}でございます")
+
+            positive_matches.append(f"{subject}が{predicate}ではございません")
+
+            positive_matches.append(f"{predicate}でないのは{subject}でございます")
+
+    for subject in non_subjects:
+        if cop == '':
+            negative_matches.append(f"{subject}")
+            negative_matches.append(f"{subject}だ")
+
+            negative_matches.append(f"{subject}が{predicate}じゃない")
+            negative_matches.append(f"{subject}が{predicate}ではない")
+
+            negative_matches.append(f"{predicate}じゃないのは{subject}だ")
+            negative_matches.append(f"{predicate}でないのは{subject}だ")
+            
+        elif cop == 'です':
+            negative_matches.append(f"{subject}です")
+
+            negative_matches.append(f"{subject}が{predicate}じゃありません")
+            negative_matches.append(f"{subject}が{predicate}ではありません")
+            negative_matches.append(f"{subject}が{predicate}じゃないです")
+            negative_matches.append(f"{subject}が{predicate}ではないです")
+
+            negative_matches.append(f"{predicate}でないのは{subject}です")
+            negative_matches.append(f"{predicate}じゃないのは{subject}です")
+
+        elif cop == 'でございます':
+            negative_matches.append(f"{subject}でございます")
+
+            negative_matches.append(f"{subject}が{predicate}ではございません")
+
+            negative_matches.append(f"{predicate}でないのは{subject}でございます")
+
+    return {'perfect': perfect_matches,
+            'positive': positive_matches,
+            'negative': negative_matches}
+
+def en_numerals1(data):
+    conj = ' and '
+
+    number = data['answer'].strip('.')
+    target = 'fruits'
+
+    non_targets = [data['context'].split(conj)[0].split(' ')[-1], data['context'].split(conj)[-1].split(' ')[1]]
+
+    perfect_matches = [number]
+    positive_matches = [f"{number} {target}"]
+    negative_matches = non_targets
+
+    return {'perfect': perfect_matches,
+            'positive': positive_matches,
+            'negative': negative_matches}
+
+def en_numerals2(data):
+    conj = ' and '
+
+    number = data['answer'].strip('.')
+    target = data['question'].split(' ')[2]
+
+    non_targets = [data['context'].split(conj)[-1].split(' ')[1]]
+
+    perfect_matches = [number]
+    positive_matches = [f"{number} {target}"]
+    negative_matches = non_targets
+
+    return {'perfect': perfect_matches,
+            'positive': positive_matches,
+            'negative': negative_matches}
+
+def de_numerals1(data):
+    conj = ' und '
+
+    number = data['answer'].strip('.')
+    target = 'Früchte'
+
+    non_targets = [data['context'].split(conj)[0].split(' ')[-1], data['context'].split(conj)[-1].split(' ')[1]]
+
+    perfect_matches = [number]
+    positive_matches = [f"{number} {target}"]
+    negative_matches = non_targets
+
+    return {'perfect': perfect_matches,
+            'positive': positive_matches,
+            'negative': negative_matches}
+
+def de_numerals2(data):
+    conj = ' und '
+
+    number = data['answer'].strip('.')
+    target = data['question'].split(' ')[2]
+
+    non_targets = [data['context'].split(conj)[-1].split(' ')[1]]
+
+    perfect_matches = [number]
+    positive_matches = [f"{number} {target}"]
+    negative_matches = non_targets
+
+    return {'perfect': perfect_matches,
+            'positive': positive_matches,
+            'negative': negative_matches}
+
+def fi_numerals1(data):
+    conj = ' ja '
+
+    number = data['answer'].strip('.')
+    target = 'hedelmää'
+
+    non_targets = [data['context'].split(conj)[0].split(' ')[-1], data['context'].split(conj)[-1].split(' ')[1].strip('.')]
+
+    perfect_matches = [number]
+    positive_matches = [f"{number} {target}"]
+    negative_matches = non_targets
+
+    return {'perfect': perfect_matches,
+            'positive': positive_matches,
+            'negative': negative_matches}
+
+def fi_numerals2(data):
+    conj = ' ja '
+
+    number = data['answer'].strip('.')
+    target = data['question'].split(' ')[2]
+
+    non_targets = [data['context'].split(conj)[-1].split(' ')[1].strip('.')]
+
+    perfect_matches = [number]
+    positive_matches = [f"{number} {target}"]
+    negative_matches = non_targets
+
+    return {'perfect': perfect_matches,
+            'positive': positive_matches,
+            'negative': negative_matches}
+
+def ja_numerals1(data):
+    number, cop = data['answer'].strip('。').split('個')
+
+    non_targets = [data['context'].split('個の')[1].split('と')[0], 
+                data['context'].split('個の')[-1].split('が')[0]]
+
+    perfect_matches = [data['answer'].strip('。 ')]
+    positive_matches = [f'{number}個']
+    negative_matches = non_targets
+
+    return {'perfect': perfect_matches,
+        'positive': positive_matches,
+        'negative': negative_matches}
+
+def ja_numerals2(data):
+    number = data['answer'].strip('。').split('個')[0]
+
+    non_targets = [data['context'].split('個の')[2].split('が')[0]]
+
+    perfect_matches = [data['answer'].strip('。 ')]
+    positive_matches = [f'{number}個']
+    negative_matches = non_targets
+
+    return {'perfect': perfect_matches,
+        'positive': positive_matches,
+        'negative': negative_matches}
